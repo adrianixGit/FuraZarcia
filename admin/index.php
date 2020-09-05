d93f1b04f55465d402d254f121775c2e6b76e562
Patryk-MacBook <pkostrzewski1@gmail.com>
2020-09-05 18:19:23 +0200

    Strona logowania + lista z menu w panelu administratora

--- admin/index.php
+++ admin/index.php
@@ -2,23 +2,119 @@
 
    session_start();
 
-           // $hash = md5('Furazarcia4252');
-
+        //    $hash = md5('Furazarcia4252');
+        // var_dump($hash);
         
 
 ?>
 
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<style>
+body {
+    font-family: "Lato", sans-serif;
+}
 
 
 
-<form method="post" action="login.php">
+.main-head{
+    height: 150px;
+    background: #FFF;
+   
+}
 
-    <input type="text" name="login">
-    <input type="password" name="password">
-    <input type="submit"  value="Zaloguj">
+.sidenav {
+    height: 100%;
+    background-color: #000;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
 
-</form>
 
+.main {
+    padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+}
+
+@media screen and (max-width: 450px) {
+    .login-form{
+        margin-top: 10%;
+    }
+
+    .register-form{
+        margin-top: 10%;
+    }
+}
+
+@media screen and (min-width: 768px){
+    .main{
+        margin-left: 40%; 
+    }
+
+    .sidenav{
+        width: 40%;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+    }
+
+    .login-form{
+        margin-top: 80%;
+    }
+
+    .register-form{
+        margin-top: 20%;
+    }
+}
+
+
+.login-main-text{
+    margin-top: 20%;
+    padding: 60px;
+    color: #fff;
+}
+
+.login-main-text h2{
+    font-weight: 300;
+}
+
+.btn-black{
+    background-color: #000 !important;
+    color: #fff;
+}
+
+</style>
+
+<div class="sidenav">
+         <div class="login-main-text">
+            <h2>Fura Żarcia<br></h2>
+            <p>Panel administracyjny</p>
+         </div>
+      </div>
+      <div class="main">
+         <div class="col-md-6 col-sm-12">
+            <div class="login-form">
+               <form method="POST" action="login.php">
+                  <div class="form-group">
+                     <label>Login</label>
+                     <input name="login" type="text" class="form-control" placeholder="User Name">
+                  </div>
+                  <div class="form-group">
+                     <label>Hasło</label>
+                     <input name="password" type="password" class="form-control" placeholder="Password">
+                  </div>
+                  <button type="submit" class="btn btn-black">Zaloguj</button>
+               </form>
+            </div>
+         </div>
+      </div>
 
         
 
--- admin/login.php
+++ admin/login.php
@@ -2,7 +2,7 @@
 
     session_start();
 
-        $link = mysqli_connect("localhost", "root", "", "admin");
+        $link = mysqli_connect("localhost", "root", "gitarasiema", "fura_zarcia");
 
             if (mysqli_connect_error()) {
 
@@ -17,7 +17,7 @@
 
             
 
-            $query = "SELECT * FROM `admin` WHERE login = '".mysqli_real_escape_string($link, $_POST['login'])."'";  
+            $query = "SELECT * FROM users WHERE login = '".$_POST['login']."'";  
 
             $result = mysqli_query($link, $query);
 
@@ -61,5 +61,50 @@
         }
 ?>
 
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+
 <p>Hello <?php echo $_SESSION['user']; ?> </p>
 <a href="logout.php">Log Out</a>
+
+<div class="container">
+    
+<h1>menu</h1>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nzwa</th>
+            <th scope="col">Skład</th>
+            <th scope="col">Cena</th>
+            <th scope="col">Pozycja</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+
+        <?php
+            $query = "SELECT * FROM menu";  
+
+            $result = mysqli_query($link, $query);
+
+            while($row = mysqli_fetch_array($result)) {
+                
+                echo '
+                <tr>
+                    <th scope="row">'.$row['id_menu'].'</th>
+                    <td>'.$row['nazwa'].'</td>
+                    <td>'.$row['sklad'].'</td>
+                    <td>'.$row['cena'].'</td>
+                    <td>'.$row['pozycja'].'</td>
+                </tr>
+                ';
+            }
+
+        ?>
+        </tbody>
+    </table>
+
+
+
+</div>
